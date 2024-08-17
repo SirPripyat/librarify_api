c1921ee605f3c0c46625eb96d261537c525422cd
leonardorossi <08.leo.rossi@gmail.com>
2024-08-17 12:55:43 -0300

    chore: initial commit with Spring Boot setup and Maven configuration

---
+++ src/main/java/com/leonardorossi/librarify/LibrarifyApplication.java
@@ -0,0 +1,15 @@
+package com.leonardorossi.librarify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SuppressWarnings("checkstyle:MissingJavadocType")
+@SpringBootApplication
+public class LibrarifyApplication {
+
+	@SuppressWarnings({"checkstyle:FileTabCharacter", "checkstyle:Indentation"})
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardorossi/librarify/LibrarifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardorossi.librarify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
