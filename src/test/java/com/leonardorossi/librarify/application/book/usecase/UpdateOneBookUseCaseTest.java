d1b20b0c582949e27a606921a3c384f7dcc647a8
leonardorossi <08.leo.rossi@gmail.com>
2024-08-19 22:36:00 -0300

    tests: add test for update one book use case

---
+++ src/test/java/com/leonardorossi/librarify/application/book/usecase/UpdateOneBookUseCaseTest.java
@@ -0,0 +1,52 @@
+package com.leonardorossi.librarify.application.book.usecase;
+
+import com.leonardorossi.librarify.application.book.gateways.BookRepository;
+import com.leonardorossi.librarify.domain.book.entity.Book;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UpdateOneBookUseCaseTest {
+  
+  @Mock
+  private BookRepository bookRepository;
+  
+  @Mock
+  private FindOneBookUseCase findOneBookUseCase;
+  
+  @InjectMocks
+  private UpdateOneBookUseCase updateOneBookUseCase;
+  
+  @Test
+  void shouldUpdateBook() {
+    Long idBook = 1L;
+    
+    Book existingBook = Book.builder()
+      .title("Harry Potter")
+      .author("J.K. Rouling")
+      .build();
+    
+    Book bookToUpdate = Book.builder()
+      .title("Harry Potter and the Philosopher's Stone")
+      .author("J.K. Rowling")
+      .build();
+    
+    when(findOneBookUseCase.find(idBook)).thenReturn(existingBook);
+    when(bookRepository.save(existingBook)).thenReturn(existingBook);
+    
+    Book updatedBook = updateOneBookUseCase.update(idBook, bookToUpdate);
+
+    assertEquals("Harry Potter and the Philosopher's Stone", updatedBook.getTitle());
+    assertEquals("J.K. Rowling", updatedBook.getAuthor());
+    
+    verify(findOneBookUseCase, times(1)).find(idBook);
+    verify(bookRepository, times(1)).save(existingBook);
+  }
+  
+}
