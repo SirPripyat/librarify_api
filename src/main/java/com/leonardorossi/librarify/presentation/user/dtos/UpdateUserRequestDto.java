5a1468abf085c9464f9cb066285752245e06bf87
leonardorossi <08.leo.rossi@gmail.com>
2024-08-21 19:23:14 -0300

    refactor: add validations

--- src/main/java/com/leonardorossi/librarify/presentation/user/dtos/UpdateUserRequestDto.java
+++ src/main/java/com/leonardorossi/librarify/presentation/user/dtos/UpdateUserRequestDto.java
@@ -1,7 +1,9 @@
 package com.leonardorossi.librarify.presentation.user.dtos;
 
 import com.leonardorossi.librarify.presentation.user.messages.UserValidationMessages;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 /**
  * DTO para a edição de um novo usuário.
@@ -13,12 +15,14 @@ import jakarta.validation.constraints.*;
  */
 public record UpdateUserRequestDto(
     @Size(max = 100, message = UserValidationMessages.NAME_SIZE)
+    @Pattern(regexp = "^[A-Za-zÀ-ÖØ-öø-ÿ\\s]+$", message = UserValidationMessages.NAME_ALPHA_ONLY)
     String name,
     
     @Email(message = UserValidationMessages.EMAIL_VALID)
     String email,
     
     @Size(max = 15, message = UserValidationMessages.PHONE_SIZE)
+    @Pattern(regexp = "^[0-9]+$", message = UserValidationMessages.PHONE_NUMERIC)
     String phone
 ) {
 }
