aa3d2ee4fddc711e2feeb48cadf15e3ca0afc619
leonardorossi <08.leo.rossi@gmail.com>
2024-08-21 18:30:06 -0300

    refactor: refactoring recommend Book by Category and add javadocs

--- src/main/java/com/leonardorossi/librarify/application/reccomendation/usecase/RecommendBookByCategoryUseCase.java
+++ src/main/java/com/leonardorossi/librarify/application/reccomendation/usecase/RecommendBookByCategoryUseCase.java
@@ -15,6 +15,13 @@ public class RecommendBookByCategoryUseCase {
   private final RecommendationRepository repository;
   private final FindLastBooksCheckedOutByUserUseCase findLastBooksCheckedOutByUserUseCase;
   
+  /**
+   * Construtor para a classe RecommendBookByCategoryUseCase.
+   *
+   * @param repository o repositório responsável por buscar recomendações
+   * @param findLastBooksCheckedOutByUserUseCase o caso de uso responsável por buscar os últimos
+   *                                             livros emprestados
+   */
   public RecommendBookByCategoryUseCase(
       RecommendationRepository repository,
       FindLastBooksCheckedOutByUserUseCase findLastBooksCheckedOutByUserUseCase
@@ -24,24 +31,41 @@ public class RecommendBookByCategoryUseCase {
   }
   
   /**
-   * Executa o caso de uso.
+   * Executa o caso de uso para recomendar um livro com base na categoria.
+   *
+   * @param idUser o ID do usuário para o qual a recomendação será feita
+   * @return a recomendação gerada
    */
   public Recommendation execute(Long idUser) {
-    List<Book> bookList = findLastBooksCheckedOutByUserUseCase.find(idUser);
-    List<Long> borrowedBooks = bookList.stream().map(Book::getId).collect(Collectors.toList());
-    
-    String category = findMainCategory(bookList);
-    
-    return repository.recommendByCategory(borrowedBooks, category);
+    List<Book> borrowedBooks = findLastBooksCheckedOutByUserUseCase.find(idUser);
+    String mainCategory = findMainCategory(borrowedBooks);
+    return repository.recommendByCategory(getBorrowedBooksIds(borrowedBooks), mainCategory);
   }
   
+  /**
+   * Encontra a categoria principal com base nos livros emprestados.
+   *
+   * @param borrowedBooks a lista de livros emprestados
+   * @return a categoria mais frequente entre os livros emprestados
+   */
   private String findMainCategory(List<Book> borrowedBooks) {
-    Map<String, Long> categoryCount = borrowedBooks.stream()
-        .collect(Collectors.groupingBy(Book::getCategory, Collectors.counting()));
-    
-    return categoryCount.entrySet().stream()
-      .max(Map.Entry.comparingByValue())
-      .map(Map.Entry::getKey)
-      .orElse(null);
+    return borrowedBooks.stream()
+        .collect(Collectors.groupingBy(Book::getCategory, Collectors.counting()))
+        .entrySet().stream()
+        .max(Map.Entry.comparingByValue())
+        .map(Map.Entry::getKey)
+        .orElse(null);
+  }
+  
+  /**
+   * Extrai os IDs dos livros emprestados.
+   *
+   * @param books a lista de livros
+   * @return uma lista com os IDs dos livros
+   */
+  private List<Long> getBorrowedBooksIds(List<Book> books) {
+    return books.stream()
+        .map(Book::getId)
+        .collect(Collectors.toList());
   }
 }
